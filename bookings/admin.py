8e0972056d9d9ea85c85df9d7fd846856b3f8468
Alisha <alishaanderssonn@icloud.com>
2024-09-30 21:23:31 +0200

    Register Booking model with custom admin configuration

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Booking
 
 # Register your models here.
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'booking_date', 'booking_details')
+    search_fields = ('user__email', 'booking_details')
+
+admin.site.register(Booking, BookingAdmin)
