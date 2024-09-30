c542e47fd71418ebc20efd9208fbb39846f47fca
Alisha <alishaanderssonn@icloud.com>
2024-09-30 21:11:00 +0200

    Add auth routes to bookings URLs

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,7 +1,14 @@
+from django.contrib.auth import views as auth_views
+from . import views
 from django.urls import path
-from .views import home_view, signup
+from django.contrib.auth.views import LogoutView
+from .views import home_view, signup, custom_login, dashboard, profile
 
-# URL patterns for the bookings app, including the signup view
+# URL patterns for the bookings app
 urlpatterns = [
     path('signup/', signup, name='signup'),
+    path('login/', views.custom_login, name='login'), 
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),  
+    path('profile/', profile, name='profile'),
 ]
