601fd17437d460041622e8cd2dcb19ea9902f261
Alisha <alishaanderssonn@icloud.com>
2024-09-30 21:19:55 +0200

    Set login/logout redirect URLs and customize message tags

--- restaurant_booking_system/settings.py
+++ restaurant_booking_system/settings.py
@@ -15,6 +15,7 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
+from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,6 +54,19 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'login'
+
+# Customize message tags 
+MESSAGE_TAGS = {
+    message_constants.DEBUG: 'debug',
+    message_constants.INFO: 'info',
+    message_constants.SUCCESS: 'success',
+    message_constants.WARNING: 'warning',
+    message_constants.ERROR: 'danger',  
+}
 
 ROOT_URLCONF = 'restaurant_booking_system.urls'
 
