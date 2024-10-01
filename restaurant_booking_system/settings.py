aef41138621d6d953a7f54b65ad4c185254e710c
Alisha <alishaanderssonn@icloud.com>
2024-10-01 19:13:45 +0200

    Add password security requirements and expiration for reset link.

--- restaurant_booking_system/settings.py
+++ restaurant_booking_system/settings.py
@@ -59,6 +59,34 @@ MIDDLEWARE = [
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 
+# Configure email backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mail.me.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'alishaanderssonn@icloud.com'
+EMAIL_HOST_PASSWORD = 'uitz-traq-mjgz-vvyx'
+
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,  
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 # Customize message tags 
 MESSAGE_TAGS = {
     message_constants.DEBUG: 'debug',
